4b8607c4b6ac1920ae4662f27ec4946f31c24aa5
Chottcodes <cwellshott@gmail.com>
2024-11-15 19:25:20 -0800

    update

---
+++ app/app.js
@@ -0,0 +1,42 @@
+let body= document.getElementById('body');
+let blueBtn=document.getElementById('blue');
+let redBtn=document.getElementById('red');
+let greenBtn=document.getElementById('green');
+let yellowBtn=document.getElementById('yellow');
+let greyBtn=document.getElementById('grey');
+let purpleBtn=document.getElementById('purple');
+let pinkBtn=document.getElementById('pink');
+let brownBtn=document.getElementById('brown');
+let orangeBtn=document.getElementById('orange');
+let goldBtn=document.getElementById('gold');
+
+blueBtn.addEventListener('click', function(){
+    body.style.backgroundColor='blue';
+});
+redBtn.addEventListener('click',function(){
+    body.style.backgroundColor='red';
+});
+greenBtn.addEventListener('click',function(){
+    body.style.backgroundColor='green';
+});
+yellowBtn.addEventListener('click',function(){
+    body.style.backgroundColor='yellow';
+});
+greyBtn.addEventListener('click',function(){
+    body.style.backgroundColor='grey';
+});
+purpleBtn.addEventListener('click',function(){
+    body.style.backgroundColor='purple';
+});
+pinkBtn.addEventListener('click',function(){
+    body.style.backgroundColor='pink';
+});
+brownBtn.addEventListener('click',function(){
+    body.style.backgroundColor='brown';
+});
+orangeBtn.addEventListener('click',function(){
+    body.style.backgroundColor='orange';
+});
+goldBtn.addEventListener('click',function(){
+    body.style.backgroundColor='gold';
+});
